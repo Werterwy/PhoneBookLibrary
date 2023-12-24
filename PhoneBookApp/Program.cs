a0964f76845796b795e0a99df1846f9d1be250ed
Orazbay <you@biy.kz>
2023-12-24 15:44:57 +0600

    Добавьте файлы проекта.

---
+++ PhoneBookApp/Program.cs
@@ -0,0 +1,166 @@
+﻿using PhoneBookLibrary;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneBookApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string projectPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Database.txt");
+            PhoneBook phoneBook = new PhoneBook(projectPath);
+            while (true)
+            {
+                Console.WriteLine("1. Показать все контакты");
+                Console.WriteLine("2. найти контакт");
+                Console.WriteLine("3. Добавить контакт");
+                Console.WriteLine("4. Редактирование контакта");
+                Console.WriteLine("5. Удаление контакта");
+                Console.WriteLine("0. Выход");
+
+                int showChoice;
+                if (!int.TryParse(Console.ReadLine(), out showChoice))
+                {
+                    Console.WriteLine("Неверный ввод, введите число.");
+                    continue;
+                }
+
+                switch (showChoice)
+                {
+                    case 1:
+                        // Вывод всех контактов
+                        List<PhoneBookContact> allContacts = phoneBook.GetAllContacts();
+
+                        foreach (var contact in allContacts)
+                        {
+                            //Console.WriteLine($"ID: {contact.Id}, Имя: {contact.Name}, Номер: {contact.PhoneNumber}, Email: {contact.Email}");
+                            Console.WriteLine(contact.ToString());
+                        }
+                        Console.WriteLine("");
+                        break;
+                    case 2:
+                        Console.WriteLine("Введите запрос для поиска(по имени, фамилии, номеру телефона или электронной почте): ");
+                        string searchQuery = Console.ReadLine();
+                        phoneBook.SearchContacts(searchQuery);
+                        Console.WriteLine("");
+                        break;
+                    case 3:
+                        // Добавление контакта
+                        Console.WriteLine("Введите имя контакта:");
+                        string firstName = Console.ReadLine();
+                        Console.WriteLine("Введите фамилию контакта:");
+                        string lastName = Console.ReadLine();
+                        Console.WriteLine("Введите номер телефона:");
+                        string phoneNumber = Console.ReadLine();
+                        Console.WriteLine("Введите электронную почту:");
+                        string email = Console.ReadLine();
+                        phoneBook.AddContact(firstName, lastName, phoneNumber, email);
+                        Console.WriteLine("Контакт добавлен.");
+                        Console.WriteLine("");
+                        break;
+                    case 4:
+                        // Редактирование контакта
+                        Console.WriteLine("Введите ID контакта для редактирования:");
+                        int idToEdit;
+                        if (!int.TryParse(Console.ReadLine(), out idToEdit))
+                        {
+                            Console.WriteLine("Неверный ввод. Пожалуйста, введите число.");
+                            continue;
+                        }
+
+                        if (phoneBook.ContactExists(idToEdit))
+                        {
+                            phoneBook.GetShowbyId(idToEdit);
+                            var buf = phoneBook.GetContactById(idToEdit);
+                            string newFirstName = buf.Name;
+                            string newLastName = buf.LastName;
+                            string newPhoneNumber = buf.PhoneNumber;
+                            string newEmail = buf.Email;
+                            bool b = true;
+                            while (b)
+                            {
+                                Console.WriteLine("Что вы хотите отредактировать:");
+                                Console.WriteLine("1. Имя");
+                                Console.WriteLine("2. Фамилия");
+                                Console.WriteLine("2. Номер телефона");
+                                Console.WriteLine("4. Электронную почту");
+                                Console.WriteLine("0. Отредактировать");
+
+                                int show;
+                                if (!int.TryParse(Console.ReadLine(), out show))
+                                {
+                                    Console.WriteLine("Неверный ввод, введите число.");
+                                    continue;
+                                }
+                               
+
+                                switch (show)
+                                {
+                                    case 1:
+                                        Console.WriteLine("Введите новое имя:");
+                                        newFirstName = Console.ReadLine();
+                                        break;
+                                    case 2:
+                                        Console.WriteLine("Введите новую фамилию:");
+                                        newLastName = Console.ReadLine();
+                                        break;
+                                    case 3:
+                                        Console.WriteLine("Введите новый номер телефона:");
+                                        newPhoneNumber = Console.ReadLine();
+                                        break;
+                                    case 4:
+                                        Console.WriteLine("Введите новую электронную почту:");
+                                        newEmail = Console.ReadLine();
+                                        break;
+                                    case 0:
+                                        phoneBook.EditContact(idToEdit, newFirstName, newLastName, newPhoneNumber, newEmail);
+                                        Console.WriteLine("Контакт отредактирован.");
+                                        Console.WriteLine("");
+                                        b = false;
+                                        break;
+                                }
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine("Контакт с таким ID не найден.");
+                        }
+                        break;
+                    case 5:
+                        // Удаление контакта
+                        Console.WriteLine("Введите ID контакта для удаления:");
+                        int idToDelete;
+                        if (!int.TryParse(Console.ReadLine(), out idToDelete))
+                        {
+                            Console.WriteLine("Неверный ввод. Пожалуйста, введите число.");
+                            continue;
+                        }
+
+                        if (phoneBook.ContactExists(idToDelete))
+                        {
+                            phoneBook.DeleteContact(idToDelete);
+                            Console.WriteLine("Контакт удален.");
+                            Console.WriteLine("");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Контакт с таким ID не найден.");
+                        }
+                        break;
+                    case 0:
+                        Console.WriteLine("Путь к базе данных: " + projectPath);
+                        Console.ReadLine();
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ PhoneBookLibrary/PhoneBook.cs
@@ -0,0 +1,224 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LiteDB;
+
+namespace PhoneBookLibrary
+{
+    public class PhoneBook
+    {
+        // private List<PhoneBookContact> contacts = new List<PhoneBookContact>();
+        private List<PhoneBookContact> contacts = null;
+
+        private readonly string path = "";
+
+        public PhoneBook(string path)
+        {
+            if (string.IsNullOrWhiteSpace(path))
+                throw new ArgumentNullException("Путь к БД должен быть заполнен");
+
+            this.path = path;
+            InitializeDatabase();
+            contacts = new List<PhoneBookContact>();
+
+            List<PhoneBookContact> allContacts = GetAllContacts();
+
+            foreach (var con in allContacts)
+            {
+                contacts.Add(con);
+            }
+        }
+
+        private void InitializeDatabase()
+        {
+            using (var db = new LiteDatabase(path))
+            {
+                var contacts = db.GetCollection<PhoneBookContact>(typeof(PhoneBookContact).Name);
+                contacts.EnsureIndex(x => x.Id, true);
+            }
+        }
+
+        public void AddContact(PhoneBookContact contact)
+        {
+            try
+            {
+                using (var db = new LiteDatabase(path))
+                {
+                    var contacts = db.GetCollection<PhoneBookContact>(typeof(PhoneBookContact).Name);
+                    contacts.Insert(contact);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ошибка при добавлении контакта: {ex.Message}");
+            }
+            contact.Id = GetNextId();
+            contacts.Add(contact);
+
+        }
+
+        public void AddContact(string firstName, string lastName, string phoneNumber, string email)
+        {
+            var newContact = new PhoneBookContact
+            {
+                Name = firstName,
+                LastName = lastName,
+                PhoneNumber = phoneNumber,
+                Email = email
+            };
+
+            AddContact(newContact);
+        }
+
+        public List<PhoneBookContact> GetAllContacts()
+        {
+            List<PhoneBookContact> allContacts = new List<PhoneBookContact>();
+
+            try
+            {
+                using (var db = new LiteDatabase(path))
+                {
+                    var contacts = db.GetCollection<PhoneBookContact>(typeof(PhoneBookContact).Name);
+                    allContacts = contacts.FindAll().ToList();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ошибка при получении всех контактов: {ex.Message}");
+            }
+
+            return allContacts;
+        }
+
+        public List<PhoneBookContact> SearchContacts(string query)
+        {
+            query = query.Trim();
+
+            List<PhoneBookContact> foundContacts = contacts
+                .Where(c => c.Name.IndexOf(query, StringComparison.OrdinalIgnoreCase) >= 0 ||
+                            c.LastName.IndexOf(query, StringComparison.OrdinalIgnoreCase) >= 0 ||
+                            c.PhoneNumber.IndexOf(query, StringComparison.OrdinalIgnoreCase) >= 0 ||
+                            c.Email.IndexOf(query, StringComparison.OrdinalIgnoreCase) >= 0)
+                .ToList();
+
+            if (foundContacts.Count > 0)
+            {
+                foreach (var contact in foundContacts)
+                {
+                    Console.WriteLine($"ID: {contact.Id}, Имя: {contact.Name} {contact.LastName}, Номер: {contact.PhoneNumber}, Email: {contact.Email}");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Контактов по запросу не найдено.");
+            }
+
+            return foundContacts;
+        }
+
+        public int GetNextId()
+        {
+            using (var db = new LiteDatabase(path))
+            {
+                var contacts = db.GetCollection<PhoneBookContact>(typeof(PhoneBookContact).Name);
+                if (contacts.Count() == 0)
+                {
+                    return 1;
+                }
+                else
+                {
+                    // Найти максимальный Id
+                    int maxId = contacts.Max(c => c.Id);
+                    return maxId;
+                }
+            }
+        }
+
+        public List<PhoneBookContact> GetContacts()
+        {
+            return contacts;
+        }
+
+        public void GetShowbyId(int id)
+        {
+            foreach (var contact in contacts)
+            {
+                if (contact.Id == id)
+                {
+                    Console.WriteLine(contact.ToString());
+                    break;
+                }
+            }
+        }
+
+        public void DeleteContact(int id)
+        {
+            try
+            {
+                using (var db = new LiteDatabase(path))
+                {
+                    var contactsCollection = db.GetCollection<PhoneBookContact>(typeof(PhoneBookContact).Name);
+                    contactsCollection.Delete(id);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ошибка при удалении контакта: {ex.Message}");
+            }
+        }
+
+        public void EditContact(int id, string firstName, string lastName, string phoneNumber, string email)
+        {
+            var thiscontact = contacts.FirstOrDefault(c => c.Id == id);
+            if (thiscontact != null)
+            {
+                thiscontact.Name = firstName;
+                thiscontact.LastName = lastName;
+                thiscontact.PhoneNumber = phoneNumber;
+                thiscontact.Email = email;
+            }
+            try
+            {
+                using (var db = new LiteDatabase(path))
+                {
+                    var contactsCollection = db.GetCollection<PhoneBookContact>(typeof(PhoneBookContact).Name);
+                    var contact = contactsCollection.FindById(id);
+
+                    if (contact != null)
+                    {
+                        contact.Name = firstName;
+                        contact.LastName = lastName;
+                        contact.PhoneNumber = phoneNumber;
+                        contact.Email = email;
+
+                        contactsCollection.Update(contact);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ошибка при редактировании контакта: {ex.Message}");
+            }
+        }
+
+        public PhoneBookContact GetContactById(int id)
+        {
+            return contacts.FirstOrDefault(c => c.Id == id);
+        }
+
+        public int GetContactCount()
+        {
+            return contacts.Count;
+        }
+
+        public bool ContactExists(int id)
+        {
+            return contacts.Any(c => c.Id == id);
+        }
+
+    }
+}
